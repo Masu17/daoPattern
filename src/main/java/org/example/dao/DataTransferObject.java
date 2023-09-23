671f694789a92c4559e331789f28927f29a2a511
Samuel Yunez <samuelyunezdaw@gmail.com>
2023-09-24 00:37:28 +0100

    Init project structure

---
+++ src/main/java/org/example/dao/DataTransferObject.java
@@ -0,0 +1,11 @@
+package org.example.dao;
+
+import org.example.model.Cliente;
+import org.example.model.Empleado;
+
+public class DataTransferObject {
+
+    Cliente cliente = new Cliente();
+    Empleado empleado = new Empleado();
+
+}
